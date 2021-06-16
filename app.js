c6bdc10a593003fae48d7f75b7d01f831e7911bf
Jerrell Gardner <jgardner@nvidia.com>
2021-06-16 16:15:56 -0500

    implemented

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
  * Step 2: Import from crypto-js module the sha265 library
  */
 
-// Write your code here
+var SHA256 = require("crypto-js/sha256");
 
 /**
  * Variables: Do not change variable values to be able to answer the quiz
@@ -16,18 +16,18 @@
 const data1 = "Blockchain Rock!";
 const dataObject = {
 	id: 1,
-  	body: "With Object Works too",
-  	time: new Date().getTime().toString().slice(0,-3)
+	body: "With Object Works too",
+	time: new Date().getTime().toString().slice(0,-3)
 };
 
 /**
  * Step 3: Add code to the `generate hash function
  * Function that generate the SHA256 Hash
- * @param {*} obj 
+ * @param {*} obj
  */
 
 function generateHash(obj) {
-	// Write your code here
+	return SHA256(JSON.stringify(obj))
 }
 
 console.log(`SHA256 Hash: ${generateHash(data1)}`);
